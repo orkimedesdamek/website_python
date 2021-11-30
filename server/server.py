1bcf21d5992c1d5a90d6dc503706a21dfd9732b0
Alex Zorin <user@MacBook-Air-Alex.local>
2021-11-30 11:37:41 +0300

    Pylint docstring & exception fix

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+"""Simple website based on Flask and Pymongo"""
 import os
 from flask import Flask, render_template, request, redirect
-import pymongo
+#import pymongo
 from pymongo import MongoClient
 
 #from dotenv import load_dotenv
@@ -16,13 +17,14 @@ APP = Flask(__name__)
 try:
     CLIENT = MongoClient('website_mongo', 27017)
     DB_STATUS = 'Connected to DB!'
-except:
+except: # pylint: disable=bare-except
     DB_STATUS = 'Failed to connect'
 
 DB = CLIENT.website_data
 TEXT_ADD = DB.text
 
 @APP.route("/action", methods=['POST'])
+"""In this function text string adds to database"""
 def action():
     #Adding a Text
     textdata = request.values.get("textdata")
@@ -31,10 +33,12 @@ def action():
 
 @APP.route('/')
 #Render index
+"""Rendering main page"""
 def index():
     return render_template('index.html')
 
 @APP.route('/db_list')
+"""In this function database contents displayed on db list page"""
 def db_list():
     #List db contents
     text_list = DB.text.find()
